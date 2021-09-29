915983a26ee63ce4a4734908eca2314a465c5f0a
Imdad Ali <imdad.ali@live.co.uk>
2021-09-29 21:21:25 +0100

    WIP Section 5

---
+++ udemy/react-the-complete-guide-incl-redux__by_maximilian-schwarzmuller/section-5/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+
+const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
+  const startEditing = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditing = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className='new-expense'>
+      {!isEditing && <button onClick={startEditing}>Add New Expense</button>}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditing}
+        />
+      )}
+    </div>
+  );
+};
+
+export default NewExpense;
